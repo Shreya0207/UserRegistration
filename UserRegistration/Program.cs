efcaad3576f8ffc3678526a7b3c47ff0aa91456c
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-18 16:00:25 +0530

    [Shreya]Add: Validating the first name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,7 +7,11 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Registration Program");
-            
+            Validation regex = new Validation();
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            bool result = regex.ValidateName(name);
+            Console.WriteLine("Name: " + result);
         }
     }
 }
--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -7,6 +7,10 @@ namespace UserRegistration
 {
     class Validation
     {
-       
+        public static string NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidateName(string name)
+        {
+            return Regex.IsMatch(name, NAME_REGEX);
+        }
     }
 }
