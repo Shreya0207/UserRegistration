a798d7f7226577466f30c50aab607ab3aaffd7b1
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-18 16:35:13 +0530

    [Shreya]Add: Validating the password rule 2

--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -7,10 +7,10 @@ namespace UserRegistration
 {
     class Validation
     {
-        public static string PASSWORD1_REGEX = "^[A-Za-z0-9!@#$%^&*]{8,}$";
+        public static string PASSWORD2_REGEX = "^(?=.*?[A-Z])[A-Za-z0-9!@#$%^&*]{8,}$";
         public bool ValidatePassword(string name)
         {
-            return Regex.IsMatch(name, PASSWORD1_REGEX);
+            return Regex.IsMatch(name, PASSWORD2_REGEX);
         }
     }
 }
