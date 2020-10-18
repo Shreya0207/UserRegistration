7637c21824673c500c22182f6c50f22ea6435c63
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-18 16:40:18 +0530

    [Shreya]Add: Validating the password rule 3

--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -7,10 +7,10 @@ namespace UserRegistration
 {
     class Validation
     {
-        public static string PASSWORD2_REGEX = "^(?=.*?[A-Z])[A-Za-z0-9!@#$%^&*]{8,}$";
+        public static string PASSWORD3_REGEX = "^(?=.*?[A-Z])(?=.*?[0-9])[A-Za-z0-9!@#$%^&*]{8,}$";
         public bool ValidatePassword(string name)
         {
-            return Regex.IsMatch(name, PASSWORD2_REGEX);
+            return Regex.IsMatch(name, PASSWORD3_REGEX);
         }
     }
 }
