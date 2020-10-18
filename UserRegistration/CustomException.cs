4274aebdf57c91fea73adf2d82d318829d26b25b
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-18 18:00:55 +0530

    [Shreya]Refactor: Throwing Custom Exceptions

---
+++ UserRegistration/CustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistration
+{
+    public class CustomException : Exception
+    {
+        public enum ExceptionType
+            {
+                INVALID_FIRST_NAME,
+                INVALID_LAST_NAME,
+                INVALID_EMAIL,
+                INVALID_MOBILENO,
+                INVALID_PASSWORD
+            }
+            private ExceptionType type;
+            public CustomException(ExceptionType Type, String message) : base(message)
+            {
+                this.type = Type;
+            }
+        }
+}
