b97766dd6cb5859584ed88ce260d1263f1e1cce6
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-20 21:22:22 +0530

    [Shreya]Refactor: Refactoring the program using Lambda Expression

--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -62,62 +62,6 @@ namespace UserRegistrationTest
             //Assert
             Assert.IsTrue(result);
         }
-        //Sad Test Cases(Test Cases Fail The Entry)
-        [TestMethod]
-        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            Validation user = new Validation();
-            string name1 = "shreya";
-            //Act
-            bool result = user.ValidateFirstName(name1);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            Validation user = new Validation();
-            string name2 = "singh";
-            //Act
-            bool result = user.ValidateFirstName(name2);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            Validation user = new Validation();
-            string name3 = "shreyasingh@gmail.com";
-            //Act
-            bool result = user.ValidateEmail(name3);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            Validation user = new Validation();
-            string name4 = "91 4569871233";
-            //Act
-            bool result = user.ValidateMobileNumber(name4);
-            //Assert
-            Assert.IsFalse(result);
-        }
-        [TestMethod]
-        public void GivenPassword_WhenValidate_ShouldReturnFalse()
-        {
-            //Arrange
-            Validation user = new Validation();
-            string name5 = "abcde@123";
-            //Act
-            bool result = user.ValidatePassword(name5);
-            //Assert
-            Assert.IsFalse(result);
-        }
         [TestMethod]
         [DataRow("abc@yahoo.com")]
         [DataRow("abc-100@yahoo.com")]
